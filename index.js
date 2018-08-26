d8d9109b451028918859c3694183914ef69ff4a2
Leslie Turis <lturis@gmail.com>
2018-08-26 19:51:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function shout(string) {
   return string.toUpperCase()
 }
+<<<<<<< HEAD
 
 function whisper(string) {
   return string.toLowerCase()
@@ -10,11 +11,20 @@ function logShout(string) {
   console.log(string.toUpperCase())
 }
 
+=======
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+>>>>>>> 6e4b3332539a00e9aba8eec88c4152b28e6c94f6
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 
 function sayHiToGrandma(string) {
+<<<<<<< HEAD
   if (string.toLowerCase() === string) {
     return "I can't hear you!"
   } else if (string.toUpperCase() === string) {
@@ -22,4 +32,19 @@ function sayHiToGrandma(string) {
   } else if (string === "I love you, Grandma.") {
     return "I love you, too."
   }
+=======
+console.log('HELLO'.toLowerCase())
+return "I can't hear you!"
+}
+function sayHiToGrandma(string.toUpperCase) {
+console.log(string)
+return "YES INDEED!"
+}
+
+
+
+function sayHiToGrandma(string) {
+  console.log("I love you, Grandma.")
+  return "I love you, too."
+>>>>>>> 6e4b3332539a00e9aba8eec88c4152b28e6c94f6
 }
