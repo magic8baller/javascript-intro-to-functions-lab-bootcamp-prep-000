20ed352e0a43d89a2ea6cf0e5a680b1294292774
Leslie Turis <lturis@gmail.com>
2018-08-26 20:12:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
+function shout(string) {
+  return string.toUpperCase;
+}
 
+shout('hello');
+
+
+function whisper(string) {
+  return string.toLowerCase();
+}
+
+whisper('HeLLo');
+
+
+function logShout(string) {
+  return string.toUpperCase();
+}
+
+console.log(logShout('HeLLo'));
+
+
+function logWhisper(string) {
+  return string.toLowerCase();
+}
+
+console.log();(logWhisper('hEllO'));
+
+
+function sayHiToGrandma(string) {
+  if (string === string.toLowerCase()) {
+    return 'I can\'t hear you!';
+  }
+  else if (string === string.toUpperCase()) {
+    return 'YES INDEED!';
+  }
+  else if (string === 'I love you.'); {
+    return 'I love you, too.';
+  }
+}
+
+sayHiToGrandma('hello');
+sayHiToGrandma('HELLO');
+sayHiToGrandma('I love you.');
